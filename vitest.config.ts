dae05ba3ddca46867522255d9482742512baf4b5
Rafa <rafaelideleon@gmail.com>
2025-01-24 22:04:29 -0800

    init setup

---
+++ vitest.config.ts
@@ -0,0 +1,22 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig, } from 'vitest/config';
+
+export default defineConfig( {
+	cacheDir: '.test-cache',
+	plugins: [react()],
+	test: {
+		coverage: {
+			reportsDirectory: './coverage',
+			all: false,
+			include: [
+				'src/*.ts',
+				'src/internal/*.ts',
+			],
+			reporter: ['text', 'html', 'json-summary',],
+		},
+		environment: 'happy-dom',
+		globals: true,
+		mockReset: true,
+		watch: false,
+	},
+}, );
