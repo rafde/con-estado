821f980e0a66f346e05bdb495ea59cfc3716e6f8
Rafa <rafaelideleon@gmail.com>
2025-02-03 22:50:05 -0800

    setup release

--- src/_internal/defaultSelector.ts
+++ src/_internal/defaultSelector.ts
@@ -1,11 +1,17 @@
 import type { ActRecord, } from '../types/ActRecord';
 import type { DS, } from '../types/DS';
+import type { EstadoHistory, } from '../types/EstadoHistory';
+import type { EstadoProps, } from '../types/EstadoProps';
+import type { Immutable, } from '../types/Immutable';
 import type { Selector, } from '../types/Selector';
 
 export default function defaultSelector<
 	State extends DS,
 	Acts extends ActRecord,
->( selectorProps: Parameters<Selector<State, Acts>>[0], ) {
+>( selectorProps: Parameters<Selector<State, Acts>>[0], ): readonly [
+	Immutable<State>,
+	EstadoProps<State, Acts> & Immutable<EstadoHistory<State>>,
+] {
 	return [
 		selectorProps.state,
 		selectorProps,
