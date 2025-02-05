bf070234556af267a6b699582cab417d59d23553
Rafa <rafaelideleon@gmail.com>
2025-02-05 11:37:53 -0800

    small cleanup

--- src/_internal/returnOnChange.ts
+++ src/_internal/returnOnChange.ts
@@ -1,6 +1,12 @@
 import { strictDeepEqual, } from 'fast-equals';
 import isPlainObject from './isPlainObject';
 
+function hasChange<P extends object, N extends object,>( prev: P, next: N, key: keyof P & keyof N, ) {
+	const nextValue = next[ key ];
+
+	return typeof nextValue !== 'function' && !strictDeepEqual( nextValue, Reflect.get( prev, key, ), );
+}
+
 export default function returnOnChange( prev: unknown, next: unknown, ) {
 	if ( prev === 'function' ) {
 		return prev;
@@ -10,16 +16,14 @@ export default function returnOnChange( prev: unknown, next: unknown, ) {
 	}
 	else if ( isPlainObject( next, ) ) {
 		for ( const key in next ) {
-			const value = next[ key ];
-			if ( typeof value !== 'function' && !strictDeepEqual( value, Reflect.get( prev as object, key, ), ) ) {
+			if ( hasChange( prev, next, key as never, ) ) {
 				return next;
 			}
 		}
 	}
 	else if ( Array.isArray( next, ) ) {
 		for ( let i = 0; i < next.length; i++ ) {
-			const value = next[ i ];
-			if ( typeof value !== 'function' && !strictDeepEqual( value, Reflect.get( prev as object, i, ), ) ) {
+			if ( hasChange( prev, next, i as never, ) ) {
 				return next;
 			}
 		}
