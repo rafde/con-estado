04019249474b310b4a4912231110809a7688a44b
Rafa <rafaelideleon@gmail.com>
2025-02-05 16:35:16 -0800

    getDeepValueParentByArray.ts remove unneeded callback

--- src/_internal/getDeepValueParentByArray.ts
+++ src/_internal/getDeepValueParentByArray.ts
@@ -6,16 +6,13 @@ import isPlainObject from './isPlainObject';
 export default function getDeepValueParentByArray<
 	State extends object,
 	ArrayPath extends StringPathToArray<NestedRecordKeys<State>> | Array<string | number>,
-	CleanKey extends ( ( key: string | number ) => string | number ) = ( key: string | number ) => string | number,
 >(
 	state: State | null | undefined,
 	arrayPath: ArrayPath,
-	cleanKey?: CleanKey,
 ) {
 	if ( state == null ) {
 		return [undefined, undefined,];
 	}
-	const _cleanKey = typeof cleanKey === 'function' ? cleanKey : ( key: string | number, ) => key;
 	let parent;
 	let value = state;
 	for ( const key of arrayPath ) {
@@ -24,7 +21,7 @@ export default function getDeepValueParentByArray<
 		if ( isObj && isArray ) {
 			break;
 		}
-		const ck = _cleanKey( key, ) as keyof typeof parent;
+		const ck = key as keyof typeof parent;
 		parent = value;
 		value = parent[ ck ];
 	}
