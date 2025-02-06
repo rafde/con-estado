d90cb60f20bc858015b8dfd7f3273b39db9a804f
Rafa <rafaelideleon@gmail.com>
2025-02-06 11:04:42 -0800

    Remove EstadoProps.ts in favor of CreateConBaseReturn

--- src/types/Selector.ts
+++ src/types/Selector.ts
@@ -1,11 +1,11 @@
+import type { CreateConBaseReturn, } from '../_internal/createConBase';
 import type { ActRecord, } from './ActRecord';
 import type { DS, } from './DS';
 import type { EstadoHistory, } from './EstadoHistory';
-import type { EstadoProps, } from './EstadoProps';
 import type { Immutable, } from './Immutable';
 
 export type Selector<
 	State extends DS,
 	Acts extends ActRecord,
 	R = unknown,
-> = ( selectorProps: EstadoProps<State, Acts> & Immutable<EstadoHistory<State>> ) => R;
+> = ( selectorProps: CreateConBaseReturn<State, Acts> & Immutable<EstadoHistory<State>> ) => R;
