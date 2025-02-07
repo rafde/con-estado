5e41b235e88ff29035a71a577362f181c0f60611
Rafa <rafaelideleon@gmail.com>
2025-02-06 18:51:56 -0800

    returnOnChange.ts ignores all function compares by making them all equal to true

--- src/_internal/returnOnChange.ts
+++ src/_internal/returnOnChange.ts
@@ -1,34 +1,20 @@
-import { strictDeepEqual, } from 'fast-equals';
-import isPlainObject from './isPlainObject';
+import { createCustomEqual, } from 'fast-equals';
 
-function hasChange<P extends object, N extends object,>( prev: P, next: N, key: keyof P & keyof N, ) {
-	const nextValue = next[ key ];
-
-	return typeof nextValue !== 'function' && !strictDeepEqual( nextValue, Reflect.get( prev, key, ), );
-}
+const isEqual = createCustomEqual( {
+	strict: true,
+	createCustomConfig() {
+		return {
+			areFunctionsEqual: () => true,
+		};
+	},
+}, );
 
 export default function returnOnChange( prev: unknown, next: unknown, ) {
-	if ( prev === 'function' ) {
-		return prev;
-	}
 	if ( prev == null ) {
 		return next;
 	}
-	else if ( isPlainObject( next, ) ) {
-		for ( const key in next ) {
-			if ( hasChange( prev, next, key as never, ) ) {
-				return next;
-			}
-		}
-	}
-	else if ( Array.isArray( next, ) ) {
-		for ( let i = 0; i < next.length; i++ ) {
-			if ( hasChange( prev, next, i as never, ) ) {
-				return next;
-			}
-		}
-	}
-	else if ( !strictDeepEqual( prev, next, ) ) {
+
+	if ( !isEqual( prev, next, ) ) {
 		return next;
 	}
 
