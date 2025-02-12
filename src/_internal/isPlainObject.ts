eec021d38d06f9f78ab352fea0664b466008c4fb
Rafa <rafaelideleon@gmail.com>
2025-02-12 00:30:02 -0800

    smaller isPlainObject.ts

--- src/_internal/isPlainObject.ts
+++ src/_internal/isPlainObject.ts
@@ -1,5 +1,3 @@
 export default function isPlainObject( value: unknown, ): value is Record<string | number, unknown> {
-	return value !== null
-		&& typeof value === 'object'
-		&& Object.getPrototypeOf( value, ) === Object.prototype;
+	return value?.constructor === Object;
 }
