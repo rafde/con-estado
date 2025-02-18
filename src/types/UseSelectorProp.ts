be27be7ea20b65ae1e498c1f10fa51c83f05a19d
Rafa <rafaelideleon@gmail.com>
2025-02-18 14:04:57 -0800

    simplify useSelector

--- src/types/UseSelectorProp.ts
+++ src/types/UseSelectorProp.ts
@@ -3,24 +3,27 @@ import type { DefaultSelector, } from './DefaultSelector';
 import type { DS, } from './DS';
 import type { Selector, } from './Selector';
 
-type DefaultUseSelector<
-	S extends DS,
-	AR extends ActRecord,
-> = {
-	useSelector: () => ReturnType<DefaultSelector<S, AR, DefaultUseSelector<S, AR>>>
-};
-
-type UseSelectorParam<
-	S extends DS,
-	AR extends ActRecord,
-> = {
-	useSelector: <Sel extends Selector<S, AR, UseSelectorParam<S, AR>>, >( select: Sel ) => ReturnType<Sel>
-};
-
 export type UseSelectorProp<
 	S extends DS,
 	AR extends ActRecord,
 > = {
-	(): ReturnType<DefaultSelector<S, AR, DefaultUseSelector<S, AR>>>
-	<Sel extends Selector<S, AR, UseSelectorParam<S, AR>>, >( select: Sel ): ReturnType<Sel>
+	/**
+	 * A function that returns a tuple containing the current state and selector props.
+	 * @overload
+	 *
+	 * @template S - The state type
+	 * @template AR - Action record type
+	 * @template DefaultSelector - The default selector type
+	 *
+	 * @returns Tuple containing the current state and selector props.
+	 */
+	useSelector(): ReturnType<DefaultSelector<S, AR, UseSelectorProp<S, AR>>>
+	/**
+	 * A function that returns a derived value from the current state.
+	 * @overload
+	 *
+	 * @typeParam {Selector} select - A selector `function` that returns a derived value
+	 * @returns The derived value returned by the selector function.
+	 */
+	useSelector<Sel extends Selector<S, AR, UseSelectorProp<S, AR>>, >( select: Sel ): ReturnType<Sel>
 };
