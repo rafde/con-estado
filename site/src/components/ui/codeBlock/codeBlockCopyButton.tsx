eb672f7b1dc9fe1f077467c26612611cd94d5b11
Rafa <rafaelideleon@gmail.com>
2025-03-03 14:01:21 -0800

    package.json clear unused or outdated dependencies

--- site/src/components/ui/codeBlock/codeBlockCopyButton.tsx
+++ site/src/components/ui/codeBlock/codeBlockCopyButton.tsx
@@ -1,6 +1,5 @@
 'use client';
 import { useCallback, useState, } from 'react';
-import { CopyToClipboard, } from 'react-copy-to-clipboard';
 import { CopyIcon, CopyCheck, } from 'lucide-react';
 
 type CodeBlockCopyButtonProps = {
@@ -9,18 +8,22 @@ type CodeBlockCopyButtonProps = {
 
 export default function CodeBlockCopyButton( props: CodeBlockCopyButtonProps, ) {
 	const [copied, setCopied,] = useState( false, );
-	const onCopy = useCallback( () => {
-		setCopied( true, );
-		setTimeout( () => setCopied( false, ), 2000, );
-	}, [], );
+	const onClick = useCallback(
+		async() => {
+			await navigator.clipboard.writeText( props.code.trim(), );
+			setCopied( true, );
+			setTimeout( () => setCopied( false, ), 2000, );
+		},
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+		[],
+	);
 
-	return <CopyToClipboard text={props.code} onCopy={onCopy}>
-		<button
-			className="absolute right-2 top-2 text-white"
-			aria-label={`Copying text: ${props.code}`}
-			type="button"
-		>
-			{copied ? <CopyCheck className="size-4 text-green-500" /> : <CopyIcon className="size-4" />}
-		</button>
-	</CopyToClipboard>;
+	return <button
+		className="absolute right-2 top-2 text-white cursor-pointer"
+		aria-label={`Copying text: ${props.code}`}
+		type="button"
+		onClick={onClick}
+	>
+		{copied ? <CopyCheck className="size-4 text-green-500" /> : <CopyIcon className="size-4" />}
+	</button>;
 }
