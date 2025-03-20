8b6022727e22ba99d7a15b13312b19a9549c322c
Rafa <rafaelideleon@gmail.com>
2025-03-20 16:14:05 -0700

    GetArrayPathValue.ts support mixed value

--- src/types/GetArrayPathValue.ts
+++ src/types/GetArrayPathValue.ts
@@ -1,9 +1,25 @@
+import type { ExtractPlainObject, } from './ExtractPlainObject';
+
+type GetObjValue<O, K extends keyof O, R,> = O[K] extends object
+	? R extends [string | number, ...Array<string | number>,]
+		? GetArrayPathValue<O[K], R>
+		: O[K]
+	: R extends Array<string | number>
+		? GetArrayPathValue<O[K], R>
+		: never;
+
 export type GetArrayPathValue<T, K extends Array<string | number>,> = K extends [infer First, ...infer Rest,]
 	? First extends keyof T
-		? Rest extends [string | number, ...Array<string | number>,]
-			? NonNullable<T[First]> extends object
-				? GetArrayPathValue<NonNullable<T[First]>, Rest>
+		? GetObjValue<T, First, Rest>
+		: First extends number
+			? T extends Array<infer U>
+				? Rest extends [string | number, ...Array<string | number>,]
+					? GetArrayPathValue<U, Rest>
+					: NonNullable<U>
 				: never
-			: T[First]
-		: never
-	: never;
+			: ExtractPlainObject<T> extends infer O
+				? First extends keyof O
+					? GetObjValue<O, First, Rest>
+					: never
+				: never
+	: T;
