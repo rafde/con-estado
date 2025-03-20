98e509cc357731d457be3ab315244c49fdca316a
Rafa <rafaelideleon@gmail.com>
2025-03-19 18:26:34 -0700

    IsPlainObject.ts

--- src/types/IsPlainObject.ts
+++ src/types/IsPlainObject.ts
@@ -1,16 +1,5 @@
-export type IsPlainObject<T,> = NonNullable<T> extends Record<string | number | symbol, unknown>
-	? T extends Array<unknown>
-		? false
-		: T extends Date
-			? false
-			: T extends Set<unknown>
-				? false
-				: T extends Map<unknown, unknown>
-					? false
-					// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
-					: T extends Function
-						? false
-						: T extends RegExp
-							? false
-							: true
-	: false;
+import type { ExtractPlainObject, } from './ExtractPlainObject';
+
+export type IsPlainObject<T,> = ExtractPlainObject<NonNullable<T>> extends never
+	? false
+	: true;
