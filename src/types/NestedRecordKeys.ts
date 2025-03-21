2391272819e14c2f497b9aa63686d640ebf780d8
Rafa <rafaelideleon@gmail.com>
2025-03-20 23:20:34 -0700

    NestedRecordKeys.ts fix array function names as keys and generic key names

--- src/types/NestedRecordKeys.ts
+++ src/types/NestedRecordKeys.ts
@@ -1,32 +1,35 @@
+import type { ADS, DS, } from './DS';
 import type { EscapeSpecial, } from './EscapeSpecial';
-import type { DS, } from './DS';
-import type { ExcludePlainObject, } from './ExcludePlainObject';
 import type { ExtractPlainObject, } from './ExtractPlainObject';
+import type { IsPlainObject, } from './IsPlainObject';
+import type { IsUnnamedKey, } from './IsUnnamedKey';
 
 type ArrayIndexKey<T,> = {
 	[K in keyof T & number]: `[${K}]` | `[${K}]${
 	( ExtractPlainObject<T[K]> extends never ? never : `.${NestedRecordKeys<ExtractPlainObject<T[K]>>}` )
-	| NestedRecordKeys<ExcludePlainObject<T[K]>>
+	| NestedRecordKeys<Extract<T[K], ADS>>
 	}`;
 }[keyof T & number];
-
-type IsUnnamedKey<T, K extends keyof T,> = string extends K ? true : false;
 
 type RecordKey<T,> = {
 	[K in keyof T & string]: IsUnnamedKey<T, K> extends true
 		? `${EscapeSpecial<K>}${
 			( ExtractPlainObject<T[K]> extends never ? never : `.${NestedRecordKeys<ExtractPlainObject<T[K]>>}` )
-			| NestedRecordKeys<ExcludePlainObject<T[K]>>
+			| NestedRecordKeys<Extract<T[K], ADS>>
 		}`
 		: (
 				EscapeSpecial<K> | `${EscapeSpecial<K>}${
 				( ExtractPlainObject<T[K]> extends never ? never : `.${NestedRecordKeys<ExtractPlainObject<T[K]>>}` )
-				| NestedRecordKeys<ExcludePlainObject<T[K]>>
+				| NestedRecordKeys<Extract<T[K], ADS>>
 			}`
 		)
 	;
 }[keyof T & string];
 
 export type NestedRecordKeys<T,> = T extends DS
-	? ArrayIndexKey<T> | RecordKey<T>
+	? T extends ADS
+		? ArrayIndexKey<T>
+		: IsPlainObject<T> extends true
+			? RecordKey<T>
+			: never
 	: never;
